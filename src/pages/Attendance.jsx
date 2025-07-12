cefeb107b138b8c51d43910d74a2ae0303784b62
amoten1984 <42321481+amoten1984@users.noreply.github.com>
2025-07-12 14:51:10 -0400

    Update Attendance.jsx

--- src/pages/Attendance.jsx
+++ src/pages/Attendance.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams, Link, useNavigate } from "react-router-dom";
 import { Home, ArrowLeft } from "lucide-react";
-import { generateWageStatement } from "./generateWageStatement";
+import { generateWageStatement } from '../utils/generateWageStatement';
 
 export default function Attendance() {
   const { staffId } = useParams();
